3db0bcb6841fdaf648c370e1f496e5cdd755bf02
DeArvis Troutman <dtroutma@stetson.edu>
2018-02-21 18:40:52 -0500

    Update with explode

--- src/testArgs.php
+++ src/testArgs.php
@@ -5,19 +5,46 @@ $val = array();
 $doubles = array();
 $singles = array();
 $flags = array();
+$count = 0;
+$dVal = false;
+$firstVal = true;
+$secondVal = false;
+$characterOne = 'a';
+$characterTwo = 'b ';
+
+ function multiexplode ($delimiters,$string)
+{
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
+
 
 foreach($argv as $a ){
     $val[] = $a;
 
     //Check if a is two ticks
-    if (strpos($a, '--') !== FALSE)
+
+
+  //Is the data a double, TWO TICKS
+    if (strpos($a, '--') !== FALSE)   // Alright its a double now what
     {
-        echo 'Found it';
-        $doubles = ['DOUBLES'][$a];
-    }
-    else
-    {
-        echo 'Not found.';
+        $exploded = multiexplode(array("--","=",","),$a);   // Gets rid of the specified characters and returns improved array
+        print_r($exploded);                                // prints array
+
+//        for ($i = 1; $i <= strlen($a);$i++)
+//        {
+//            $doubles[1] = $exploded[$i + 1];
+//        }
+//        print_r($doubles);
+//        0
+//        1   names
+//        2   good
+//
+// foreach ($exploded as $e)
+//        {
+//            $doubles = [$exploded[1]] = $e;
+//        }
     }
 }
 

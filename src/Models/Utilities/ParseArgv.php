a878e71c10a93a891619ed0ef2b3afb69da0ecf5
DeArvis Troutman <dtroutma@stetson.edu>
2018-02-22 03:45:43 -0500

    Updated on Doubles

--- src/Models/Utilities/ParseArgv.php
+++ src/Models/Utilities/ParseArgv.php
@@ -11,25 +11,26 @@ $val = array();
 $doubles = array();
 $singles = array();
 $flags = array();
-$count = 1;
 $clicker = 0;
-$dVal = false;
-$firstVal = true;
-$secondVal = false;
-$characterOne = 'a';
-$characterTwo = 'b ';
+
 
 // Function which allows the user to explode using multiple chars
 foreach($argv as $a ){
     $val[] = $a;
 
-
     //Is the data a double, TWO TICKS
     if (strpos($a, '--') !== FALSE)   // Alright its a double now what
     {
         $tempTwo;
-        $temp = explode("=", $a); // --good grades
+        $temp = explode("=", $a);
         $temp1 = str_replace("--", "", $temp[0]);
+
+        foreach ($temp as $t)
+        {
+            $v =  explode(",", $t);
+        }
+
+        $arguments[] = count($v);  // Array of how many arguments
         $c = count($temp);
         $clicker += 1;
 
@@ -39,14 +40,25 @@ foreach($argv as $a ){
         $doubles[$temp1][] = $tempTwo;
     }
 
+//    if(strpos($a, '-') !== FALSE)
+//    {
+//
+//    }
+
+
+
+
+
+
+
+    // End of for loop
     }
-echo("DOUBLE\n");
-print_r($doubles);
+echo("");
+print_r($a);
 
 
+// If the data is a single Flag
 
-//  DOUBLES ’grades’ => ’good’ (1 argument)
-// ’names’ => [0] ’Dominic’, [1] ’Mimi’, [2] ’Luke’ (3 arguments)
 
 
 
